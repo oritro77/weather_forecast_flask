7f97d06dc8199243146c334bf4980be259965193
aritra banerjee <oritro.75b@gmail.com>
2018-08-03 22:56:51 +0600

    fixed error response for weather forecast

--- get_weather.py
+++ get_weather.py
@@ -12,7 +12,11 @@ class Weather():
         """
 
         full_url = config.DARK_SKY_FORECAST_API + config.DARK_SKY_API_SECRET_KEY + '/' + str(lat) + ',' + str(lon) + '?units='+unit
+        print(full_url)
         forecast_weather = requests.get(url=full_url)
-        return forecast_weather.json()
-
-
+        status_code = forecast_weather.status_code
+        response_json = forecast_weather.json()
+        if status_code == 200:
+            return response_json
+        else:
+            raise response_json["error"]
